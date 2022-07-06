b77eb47e524324e94495094acb62201d8d302cae
Vika$ <Vika$@192.168.0.101>
2022-07-06 18:29:03 +0530

    By Vika$

---
+++ GitHub/src/Indus/GitDemo.java
@@ -0,0 +1,14 @@
+package Indus;
+
+public class GitDemo 
+{
+public void m1()
+{
+	System.out.println("m1 Method");
+}
+	
+	public static void main(String[] args) {
+	System.out.println("Git Demo");
+}
+
+}
