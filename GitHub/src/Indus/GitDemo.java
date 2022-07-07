5e18ee43104968e706631493279eb6f6a6f208c3
Vika$ <Vika$@192.168.0.101>
2022-07-07 08:49:32 +0530

    By Ashu

--- GitHub/src/Indus/GitDemo.java
+++ GitHub/src/Indus/GitDemo.java
@@ -6,6 +6,11 @@ public void m1()
 {
 	System.out.println("m1 Method");
 }
+
+public void m2()
+{
+	System.out.println("m1 Method");
+}
 	
 	public static void main(String[] args) {
 	System.out.println("Git Demo");
