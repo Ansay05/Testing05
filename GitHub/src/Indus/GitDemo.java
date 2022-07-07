95729b578b946169504d875977af66eb558434ba
Ansay05 <107750806+Ansay05@users.noreply.github.com>
2022-07-07 08:45:31 +0530

    Update GitDemo.java
    By Vikku

--- GitHub/src/Indus/GitDemo.java
+++ GitHub/src/Indus/GitDemo.java
@@ -7,6 +7,12 @@ public void m1()
 	System.out.println("m1 Method");
 }
 	
+	public void m2()
+{
+	System.out.println("m2 Method");
+}
+	
+	
 	public static void main(String[] args) {
 	System.out.println("Git Demo");
 }
